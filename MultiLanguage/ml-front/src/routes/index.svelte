8bbb101a3fe50ef9294a6181e073cd7805a764b6
Maciej Lisowski <macieejl00@gmail.com>
2022-08-04 21:27:06 +0200

    implemented filling variables in labels
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

--- MultiLanguage/ml-front/src/routes/index.svelte
+++ MultiLanguage/ml-front/src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { Label } from "../lib/Label"
-    export let labels: Label[]
+    export let labels: Label[] = []
+    let test = "Hey, I have been called from Spring!"
 
     async function getLabel() {
         try {
@@ -15,13 +16,23 @@
     }
 
     const labelPromise = getLabel() ?? { labels }
+
+    const fillVars = (content: string, vars: {} = {}) => {
+        (Object.keys(vars) as Array<keyof typeof vars>).map((k) => {
+            const regex = new RegExp('{{'+k+'}}', "g")
+            content = content.replace(regex, vars[k])
+        })
+
+        return content
+    }
+
 </script>
 
 {#await labelPromise}
     <h1>Waiting for data</h1>
 {:then data}
     {#each data as label}
-        <h1>{label.content}</h1>
+        <h1>{fillVars(label.content, {test})}</h1>
     {/each}
 {:catch}
     <h1>Something went wrong</h1>
